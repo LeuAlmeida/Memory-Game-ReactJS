7e853e0d9083df94d1cb20f5290c383e296ddcc8
Léu Almeida <leo@webid.net.br>
2019-09-25 17:02:29 -0300

    Create app in containers

---
+++ src/containers/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Routes from "../Routes"
+
+const App = () => (
+    <Router>
+        <Routes />
+    </Router>
+)
+
+export default App
---
+++ src/containers/Home/HomePage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HomePage = () => (
+    <h1>Olá mundo</h1>
+)
+
+export default HomePage
