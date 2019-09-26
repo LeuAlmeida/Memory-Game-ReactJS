7aabfe8421b2dedb6b960a6d58bae981ad082736
Léu Almeida <leo@webid.net.br>
2019-09-26 11:24:48 -0300

    Update App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import CssBaseline from "@material-ui/core/CssBaseline"
 
 import Routes from "../Routes"
 
 const App = () => (
-    <Router>
-        <Routes />
-    </Router>
+    <>
+        <CssBaseline />
+        <Router>
+            <Routes />
+        </Router>
+    </>
 )
 
 export default App
