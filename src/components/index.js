f83ad17de26c87a477f5db87058080ae33de80b8
Léu Almeida <leo@webid.net.br>
2019-09-30 17:52:53 -0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Dashboard from "./Dashboard"
 import DashboardMenu from "./DashboardMenu"
 import DashboardButton from "./DashboardButton"
+import Board from "./Board"
+import Card from "./Card"
 
-export { Dashboard, DashboardMenu, DashboardButton }
+export { Dashboard, DashboardMenu, DashboardButton, Board, Card }
