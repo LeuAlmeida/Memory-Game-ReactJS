7aedf466b6e8f3f74f2bc14452cb497a7c4c612a
Léu Almeida <leo@webid.net.br>
2019-09-30 17:52:39 -0300

    Update DashboardButton.js

--- src/components/DashboardButton.js
+++ src/components/DashboardButton.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Button from "@material-ui/core/Button"
+import { Link } from "react-router-dom"
 
-const DashboardButton = ({ children }) => (
-    <Button variant="outlined" color="primary" size="large" fullWidth>
+const DashboardButton = ({ children, to }) => (
+    <Button component={Link} to={to} variant="outlined" color="primary" size="large" fullWidth>
         {children}
     </Button>
 )
