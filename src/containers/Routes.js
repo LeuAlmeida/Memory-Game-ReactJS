ce57602fcbe4d77907572aa0c98fb89126a817a7
Léu Almeida <leo@webid.net.br>
2019-09-30 17:53:33 -0300

    Update Routes.js

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import HomePage from "./Home/HomePage"
+import GamePage from "./Game/GamePage"
 
 const Routes = () => (
     <Switch>
         <Route path="/" exact component={HomePage} />
+        <Route path="/game" exact component={GamePage} />
     </Switch>
 )
 
